b7f253561d7c9b6a37cd73e40d3ec962d5dab4bd
Mathy4502 <mathy4502@gmail.com>
2020-11-30 17:17:13 +0100

    Table Paris + pariSequence

--- EssaiBDD/CréationBDD.sql
+++ EssaiBDD/CréationBDD.sql
@@ -1,27 +1,52 @@
 
-CREATE SEQUENCE public.newSequence
+CREATE SEQUENCE public.userSequence
 INCREMENT 1
     START 1
     MINVALUE 1
     MAXVALUE 9223372036854775807
     CACHE 1;
 
---ALTER SEQUENCE public."seq_name"
---    OWNER TO postgres;
-
-GRANT ALL ON SEQUENCE newSequence TO "newOwner";
-
-
-
 
 CREATE TABLE Utilisateur
 (
-    idUser integer NOT NULL DEFAULT nextval('newSequence'
-    ::regclass)
+    idUser integer NOT NULL DEFAULT nextval('userSequence'
+    ::regclass),
     pseudo CHAR,
     mdp CHAR,
     mail CHAR,
     argent FLOAT,
     naissance DATE,
-    statut CHAR,
+    statut CHAR
 );
+
+
+    CREATE SEQUENCE public.pariSequence
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+
+
+    CREATE TABLE Pari
+    (
+        idParis integer NOT NULL DEFAULT nextval('pariSequence'
+        ::regclass),
+        intitulé CHAR,
+        categorie CHAR,
+        choix1 CHAR,
+        choix2 CHAR,
+        choix3 CHAR,
+        resultatMatch CHAR,
+        coteChoix1 FLOAT,
+        coteChoix2 FLOAT,
+        coteChoix3 FLOAT,
+        coteAuto BOOLEAN,
+        dateMatch DATE,
+        dateStartParis DATE,
+        dateEndParis DATE,
+        gainBanque FLOAT,
+        perteBanque FLOAT
+
+    );
